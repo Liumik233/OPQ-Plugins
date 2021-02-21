f4e9deb68ec5f38e3e302d9884541c74d4b1a70e
Liumik233 <liumikhazne@gmail.com>
2021-02-21 15:06:05 +0800

    fix

--- Aria2M-OPQ/aria2.go
+++ Aria2M-OPQ/aria2.go
@@ -100,9 +100,8 @@ func (a *aria2c) Filestatus(gid string) (string, error) {
 	spi, err := strconv.ParseInt(rsp.DownloadSpeed, 10, 64)
 	toi, err := strconv.ParseFloat(rsp.TotalLength, 64)
 	cpi, err := strconv.ParseFloat(rsp.CompletedLength, 64)
-	i := cpi / toi * 10
 	if rsp.BitTorrent.Info.Name != "" {
-		return "文件名：" + rsp.BitTorrent.Info.Name + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(i*10), 10) + "%\n" + pr(int(i)), err
+		return "文件名：" + rsp.BitTorrent.Info.Name + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(cpi/toi*100), 10) + "%\n" + rsp.CompletedLength + "/" + rsp.TotalLength, err
 	} else {
 		if len(rsp.FollowedBy) == 1 {
 			rsp, err = a.a.TellStatus(rsp.FollowedBy[0])
@@ -110,9 +109,9 @@ func (a *aria2c) Filestatus(gid string) (string, error) {
 				return "err", err
 				log.Println(err)
 			}
-			return "文件名：" + rsp.BitTorrent.Info.Name + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(cpi/toi*100), 10) + "%\n" + pr(int(i)), err
+			return "文件名：" + rsp.BitTorrent.Info.Name + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(cpi/toi*100), 10) + "%\n" + rsp.CompletedLength + "/" + rsp.TotalLength, err
 		} else {
-			return "文件名：" + strings.Trim(rsp.Files[0].Path, rsp.Dir) + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(cpi/toi*100), 10) + "%\n" + pr(int(i)), err
+			return "文件名：" + strings.Trim(rsp.Files[0].Path, rsp.Dir) + "\n下载状态：" + rsp.Status + "\n下载速度：" + strconv.FormatInt(spi/1024, 10) + "KB/s\n下载进度：" + strconv.FormatInt(int64(cpi/toi*100), 10) + "%\n" + rsp.CompletedLength + "/" + rsp.TotalLength, err
 		}
 	}
 }
